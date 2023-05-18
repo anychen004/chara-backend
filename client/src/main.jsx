a0f8f70f8c95eb657c2a99ded3bb16f311f2978d
Anya C <58009794+anychen004@users.noreply.github.com>
2023-05-17 20:40:49 -0700

    update

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,9 +3,11 @@ import ReactDOM from 'react-dom/client'
 import Background from './background'
 import App from './app'
 import './index.css'
+import Asset from './misc_assets'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    <Asset /> {/* TODO: CONSOLIDATE ASSET & BACKGROUND INTO TWO CYLINDERS*/}
     <App />
     <Background />
   </React.StrictMode>,
--- client/src/misc_assets.jsx
+++ client/src/misc_assets.jsx
@@ -2,35 +2,38 @@ import { useRef, useState } from 'react'
 import { Canvas, useFrame } from '@react-three/fiber'
 import { OrbitControls } from '@react-three/drei'
 
-function Box(props) {
+function Pointer(props) {
   // This reference gives us direct access to the THREE.Mesh object
   const ref = useRef()
   // Hold state for hovered and clicked events
   const [hovered, hover] = useState(false)
   const [clicked, click] = useState(false)
   // Subscribe this component to the render-loop, rotate the mesh every frame
-  useFrame((state, delta) => (ref.current.rotation.y += delta*0.1))
+  useFrame((state, delta) => (ref.current.rotation.z -= delta*0.5))
   // Return the view, these are regular Threejs elements expressed in JSX
+  
   return (
     <mesh
       {...props}
-      ref={ref}>
-      <boxGeometry args={[5, 0.1, 5]} />
-      <meshStandardMaterial color={'#4e6081'} />
+      ref={ref}
+      onPointerOver={(event) => hover(true)}
+      onPointerOut={(event) => hover(false)}>
+      <cylinderGeometry args={[1, 1, 0.25, 3]} /> {/* triangular prism. see if I can get to fill screen? */}
+      <meshStandardMaterial color={hovered ? "red" : "#F2F2F2"} />
     </mesh>
   )
 }
 
-export default function Background() {
+export default function Asset() {
   return (
-    <div className='background'>
+    <div className='foreground'>
     <Canvas>
-        <perspectiveCamera fov={60} />
+        <perspectiveCamera fov={60}/>
         <ambientLight intensity={0.5} />
         <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} />
         <pointLight position={[-10, -10, -10]} />
-        <Box position={[0, -2.5, 0]} />
-        <OrbitControls enablePan={false} enableZoom={false} /> {/* the rotate/zoom functionality on your canvas. comment out if it gets too much lmao */}
+        <Pointer position={[0, 0, 0]} rotation-x={3.141592653589793238462643383279502884197*0.5} /> {/* too lazy to import MATH PI */}
+        <OrbitControls enablePan={false} enableZoom={false} enableRotate={false} /> {/* the rotate/zoom functionality on your canvas. comment out if it gets too much lmao */}
     </Canvas>
   </div>
   )
